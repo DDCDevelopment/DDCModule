d83cdf1b488f54b136e8fd336d49ee060478f0a3
emil-hp <emilien1.taillet@epitech.eu>
2020-11-16 12:39:37 +0100

    <Add> Adding .next to gitignore and editing pages's file

--- pages/_app.js
+++ pages/_app.js
@@ -7,18 +7,16 @@ import translations from '@shopify/polaris/locales/en.json';
 class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props;
-        return ( <
-            React.Fragment >
-            <
-            Head >
-            <
-            title > Sample App < /title> <
-            meta charSet = "utf-8" / >
-            <
-            /Head> <AppProvider i18n={translations}><
-            Component {...pageProps }
-            /> < /AppProvider > < /
-            React.Fragment >
+        return ( 
+            <React.Fragment>
+                <Head>
+                    <title> Sample App </title>
+                    <meta charSet = "utf-8" />
+                </Head>
+                <AppProvider i18n={translations}>
+                    <Component {...pageProps }/>
+                </AppProvider> 
+            </React.Fragment>
         );
     }
 }
