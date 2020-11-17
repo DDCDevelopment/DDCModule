e1a37c93e2a1cd886c375d767f130f51c746836e
emil-hp <emilien1.taillet@epitech.eu>
2020-11-17 15:57:16 +0100

    <Add> popup

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,11 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import '@shopify/polaris/dist/styles.css';
+import { Provider } from '@shopify/app-bridge-react';
 import translations from '@shopify/polaris/locales/en.json';
+import ClientRouter from '../components/ClientRouter';
 //libs
+import Cookies from 'js-cookie';
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -11,15 +14,19 @@ import ReactDOM from "react-dom";
 class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props;
+        const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
         return ( 
             <React.Fragment>
                 <Head>
                     <title> Sample App </title>
                     <meta charSet = "utf-8" />
                 </Head>
-                <AppProvider i18n={translations}>
-                    <Component {...pageProps }/>
-                </AppProvider> 
+                <Provider config={config}>
+                    <ClientRouter />
+                    <AppProvider i18n={translations}>
+                        <Component {...pageProps }/>
+                    </AppProvider>
+                </Provider>
             </React.Fragment>
         );
     }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,90 @@
-const Index = () => (
-    <div>
-        <p> Sample app using React and Next.js </p>
-    </div>
-);
+import { Layout, Page, EmptyState } from '@shopify/polaris';
+import React from "react";
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import Modal from 'react-awesome-modal';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
+
+
+class Index extends React.Component {
+    state = { open: false };
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible : false
+        }
+    }
+
+    openModal() {
+        this.setState({
+            visible : true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            visible : false
+        });
+    }
+
+    render() {
+        return (
+            <>
+            <div>
+                <Page>
+                    <TitleBar
+                    title="Sample App"
+                    primaryAction={{
+                        content: 'Select products',
+                        onAction: () => this.setState({ open: true }),
+                    }}
+                    />
+                    <ResourcePicker
+                    resourceType="Product"
+                    showVariants={false}
+                    open={this.state.open}
+                    onSelection={(resources) => this.handleSelection(resources)}
+                    onCancel={() => this.setState({ open: false })}
+                    />
+                    <Layout>
+                        <EmptyState
+                            heading="Discount your products temporarily"
+                            action={{
+                            content: 'Select products',
+                            onAction: () => this.setState({ open: true }),
+                            }}
+                            image={img}
+                        >
+                            <p>Select products to change their price temporarily.</p>
+                        </EmptyState>
+                        <input type="button" value="Open" onClick={() => this.openModal()} />
+                        <Modal 
+                            visible={this.state.visible}
+                            width="1000"
+                            height="600"
+                            effect="fadeInUp"
+                            onClickAway={() => this.closeModal()}
+                        >
+                            <div>
+                                <h1>Title</h1>
+                                <p>Some Contents</p>
+                                <a href="javascript:void(0);" onClick={() => this.closeModal()}>Close</a>
+                            </div>
+                        </Modal>
+                    </Layout>
+                </Page>
+            </div>
+            </>
+            
+        );
+    }
+    handleSelection = (resources) => {
+        const idsFromResources = resources.selection.map((product) => product.id);
+        this.setState({ open: false })
+        console.log(idsFromResources)
+    };
+}
+
 
 export default Index;
